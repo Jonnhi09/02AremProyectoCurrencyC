f024e440d9a9bad55b5bff07ddcf0115f12e61b4
Jonathan Prieto <jonnhi.prietou@gmail.com>
2018-10-26 05:17:50 -0500

    Front y Readme actualizados

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
-# 02AremProyectoCurrencyC
+# Escuela Colombiana de Ingeniería Julio Garavito
+## Arquitecturas empresariales
+### Jonathan Prieto 
+#### Segundo proyecto - Convertidor de monedas
+#### [Link AWS](http://currencyc-jonathan-prieto.s3-website-us-east-1.amazonaws.com)
+#### [Link Artículo](https://docs.google.com/document/d/1eEA_byE8dkwNHvZAMrbBF6oP7pY_s5g_t6CH16jpVF4/edit?usp=sharing)
+---
+##### Descripción
+
+Usted es un arquitecto en una organización que proporciona soluciones modernas y fáciles de escalar.
+
+Un cliente del sector financiero desea construir un portal  que exponga servicios para el manejo rápido de las transacciones que efectúa la mesa de dinero. El primer conjunto de servicios que se incluirá en la plataforma será el de tasas de cambio. Usted debe diseñar e implementar una aplicación para la consulta y búsqueda de tasas de cambio y para la conversión de monedas. La aplicación debe construirse con microservicios usando la infraestructura de AWS lambda, AWS API Gateway y otros que usted considere. El aplicativo debe tener una interfaz de usuario simple, el servidor es un servidor de publicación de tasas de cambio y el canal de comunicación es TCP o HTTP.
+
+La aplicación podrá extenderse con otros conjuntos de microservicios, es decir su implementación inicial incluirá los requerimientos de tasa de cambio, pero debería poder extenderse a otros servicios financieros, por ejemplo, para negociar bonos, o bitcoins.
+
+
+El servidor expondría diferentes servicios para el consumo de la aplicación cliente, lista de tasas de cambio para diferentes monedas, búsqueda del tipo de cambio para una moneda específica y un conversor de moneda para convertir una cantidad específica de dinero de una moneda a otra. Utilice una de las API de conversión de moneda pública (https://openexchangerates.org/signup/free,https://currencylayer.com/documentation, etc.) en el lado del servidor para obtener las últimas tasas de conversión.
+
+La aplicación cliente debe proporcionar una interfaz simple y clara para que el usuario final realice diferentes funcionalidades expuestas por el servidor, listando las monedas con sus tasas de cambio, el cuadro de búsqueda de una moneda específica, la caja de cambio de moneda y un botón de actualización para actualizar las tarifas del servidor .
+
+La aplicación cliente debe ser responsive, cualquier congelación en la aplicación cliente no es aceptable, las operaciones de la red deben ser operaciones sin bloqueo, se debe notificar al usuario que hay una llamada del servidor y recibir una notificación una vez que se reciba la respuesta y se presente sin bloqueos.
+
+Debe utilizar tecnologías Java y JS modernas para diseñar e implementar la aplicación con las especificaciones mencionadas.
+
+- En la raíz del repositorio se podrá encontrar el artículo y el modelo en astah del proyecto.
---
+++ js/controller.js
@@ -0,0 +1,93 @@
+var mapLatest = {};
+var mapCurrencies = {};
+var tasaActual;
+
+function getLatest() {
+    axios.get('https://openexchangerates.org/api/latest.json?app_id=41ee871aa5954e9bbb414f38703cd0d2').then(function (response) {
+        for (i in response.data["rates"]) {
+            mapLatest[i] = response.data["rates"][i];
+        }
+        setMessageStatus(response.status + " " + response.statusText);
+        setTasaActual();
+    }).catch(function (error) {
+        console.log(error);
+    }).then(function () {
+        setMessageStatus(response.status + " " + response.statusText);
+    });
+}
+
+function getCurrents() {
+    axios.get('https://openexchangerates.org/api/currencies.json?app_id=41ee871aa5954e9bbb414f38703cd0d2').then(function (response) {
+        var base = document.getElementById("SelectBase");
+        var cotizada = document.getElementById("SelectCotizada");
+        for (i in response.data) {
+            mapCurrencies[i] = response.data[i];
+            var option = document.createElement("option");
+            var option2 = document.createElement("option");
+            option.text = response.data[i];
+            option2.text = response.data[i];
+            if (i == "USD") {
+                option.setAttribute("selected", "selected")
+            }
+            if (i == "COP") {
+                option2.setAttribute("selected", "selected")
+            }
+            base.add(option);
+            cotizada.add(option2);
+        }
+        setMessageStatus(response.status + " " + response.statusText);
+    }).catch(function (error) {
+        console.log(error);
+    }).then(function () {
+        setMessageStatus(response.status + " " + response.statusText);
+    });
+}
+
+function setValor(num, id1, id2) {
+    console.log(num);
+    var num = parseInt(num);
+    if (!isNaN(num)) {
+        console.log(num);
+        if (id1 == "InputBase") {
+            console.log("cambia cotizada");
+            document.getElementById(id2).value = (num * tasaActual).toFixed(4);
+        } else {
+            console.log("cambia base");
+            document.getElementById(id2).value = (num / tasaActual).toFixed(4);
+        }
+    } else {
+        console.log("error");
+        document.getElementById(id1).setAttribute("value", 0);
+        document.getElementById(id2).setAttribute("value", 0);
+    }
+}
+
+function setTasaActual() {
+    var base;
+    var cotizada;
+    var selectBase = document.getElementById("SelectBase");
+    var valueIdBase = selectBase.options[selectBase.selectedIndex].value;
+    var selectCotizada = document.getElementById("SelectCotizada");
+    var valueIdCotizada = selectCotizada.options[selectCotizada.selectedIndex].value;
+    for (i in mapCurrencies) {
+        if (mapCurrencies[i] == valueIdBase) {
+            base = mapLatest[i];
+        }
+        if (mapCurrencies[i] == valueIdCotizada) {
+            cotizada = mapLatest[i];
+        }
+    }
+    tasaActual = cotizada / base;
+    var alert = document.getElementById("Tasa");
+    alert.innerHTML = "1 " + valueIdBase + " equals " + tasaActual + " " + valueIdCotizada;
+}
+
+function setMessageStatus(message) {
+    var alert = document.getElementById("Status");
+    alert.innerHTML = "Http status: " + message;
+}
+
+function main() {
+    getCurrents();
+    getLatest();
+}
